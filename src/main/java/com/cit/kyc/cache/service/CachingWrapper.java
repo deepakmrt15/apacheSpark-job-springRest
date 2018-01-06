dfc0a4b3f1124091129422062cde5f4fbc07bc16
Deepak <deepak.sharma@bankofthewest.com>
2018-01-05 16:46:48 -0800

    Joined code added

--- src/main/java/com/cit/kyc/cache/service/CachingWrapper.java
+++ src/main/java/com/cit/kyc/cache/service/CachingWrapper.java
@@ -29,22 +29,54 @@ public class CachingWrapper {
         try{
             Dataset<Row> partyDF =  dfCreator.partyDF(session);
             Dataset<Row> relatedPartyDF =  dfCreator.relatedPartyDF(session);
+            //1. relationship join--party and related parties
             Dataset<Row> party2RelatedPartyDF =
-                    partyDF.alias("e").join(relatedPartyDF.alias("d"), partyDF.col("rowid_object")
+                    partyDF.join(relatedPartyDF, partyDF.col("rowid_object")
                             .equalTo(relatedPartyDF.col("rltd_prty_id")),"inner");
                             //.select("e.EMPID as eEMP, e.NAME as eName, d.DNAME as dName, d.EMPID as dEMPID ");// ,"inner"
-            long joindCnt = party2RelatedPartyDF.count();
+            long joindP2RlPCnt = party2RelatedPartyDF.count();
 
-            System.out.println("party2RelatedPartyDF count: " + String.valueOf(joindCnt));
-            party2RelatedPartyDF//"prty_id", "prty_rltd_prty_typ_cd"
-                    .foreach((Row row) -> {
-                        System.out.println("prty_id: " + row.getAs("rowid_object")
-                                +", rltd_prty_id: "+
-                                row.getAs("rltd_prty_id"));
+            System.out.println("party2RelatedPartyDF count: " + String.valueOf(joindP2RlPCnt));
 
-                    });
+//            party2RelatedPartyDF//"prty_id", "prty_rltd_prty_typ_cd"
+//                    .foreach((Row row) -> {
+//                        System.out.println("prty_id: " + row.getAs("rowid_object")
+//                                +", rltd_prty_id: "+
+//                                row.getAs("rltd_prty_id"));
+//
+//                    });
 
-            resp.append(String.valueOf(joindCnt));
+
+            Dataset<Row> partyAddressDF =  dfCreator.partyAddressDF(session);
+
+            //2. relationship -party and address
+            Dataset<Row> relParty2AddressDF =
+                    partyAddressDF.join(relatedPartyDF, partyDF.col("prty_id")
+                            .equalTo(relatedPartyDF.col("rltd_prty_id")),"inner");
+            long party2AddressDFCnt = relParty2AddressDF.count();
+
+            System.out.println("relParty2AddressDF count: " + String.valueOf(party2AddressDFCnt));
+
+            //3. join party to screening
+            Dataset<Row> screeningInfoDF =  dfCreator.screeningInfoDF(session);
+
+            //2. relationship -party and address
+            Dataset<Row> relParty2ScreeningDF =
+
+                    partyAddressDF.join(relatedPartyDF, partyDF.col("prty_id")
+                            .equalTo(relatedPartyDF.col("rltd_prty_id")),"inner");
+            long party2ScreeningDFCnt = relParty2ScreeningDF.count();
+
+            System.out.println("party2ScreeningDFCnt count: " + String.valueOf(party2ScreeningDFCnt));
+
+
+
+
+
+            //resp.append(String.valueOf(party2ScreeningDFCnt));
+            resp.append("\n party2RelPartyJoinCount: "+String.valueOf(joindP2RlPCnt));
+            resp.append("\n party2AddressJoinCount: "+String.valueOf(party2AddressDFCnt));
+            resp.append("\n party2ScreeningJoinCount: "+String.valueOf(party2ScreeningDFCnt));
 
            // return resp.toString();
 //            Dataset<Row> partyAddressDF =  dfCreator.partyAddressDF(session);
@@ -55,7 +87,7 @@ public class CachingWrapper {
 //            classifDF.createOrReplaceTempView("CLASSIFICATION");
 //            Dataset<Row> orgDF =  dfCreator.orgDF(session);
 //            orgDF.createOrReplaceTempView("ORGN");
-//            Dataset<Row> screeningInfoDF =  dfCreator.screeningInfoDF(session);
+//
 //            screeningInfoDF.createOrReplaceTempView("SCREENING");
 
 
